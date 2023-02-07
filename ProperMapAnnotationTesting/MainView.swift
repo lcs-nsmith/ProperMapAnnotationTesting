6469abc9d8324d78fd5b0167227cc1c5c93402ad
Nathan Smith <nsmith@lcs.on.ca>
2023-02-07 17:22:12 -0500

    ERROR/BUG found: This is where there is a purple danger sign and the error message "Modifying state during view update, this will cause undefined behavior." appears. I am not sure what causes it, but when running it will sometime start repeatedly zooming all the way out into space and gets stuck glitching around.

--- ProperMapAnnotationTesting/MainView.swift
+++ ProperMapAnnotationTesting/MainView.swift
@@ -19,17 +19,43 @@ struct Position: Identifiable {
 struct MainView: View {
     @State private var region = MKCoordinateRegion(center: CLLocationCoordinate2D(latitude: 44.439230, longitude: -78.265300), span: MKCoordinateSpan(latitudeDelta: 0.004, longitudeDelta: 0.004))
     
-    var bryanJonesTheatre = Position(name: "Bryan Jones Theatre", coordinate: CLLocationCoordinate2D(latitude: 44.439680, longitude: -78.265033))
+//    var bryanJonesTheatre = Position(name: "Bryan Jones Theatre", coordinate: CLLocationCoordinate2D(latitude: 44.439680, longitude: -78.265033))
+    
+    var whereabout = [
+    
+        Position(name: "Bryan Jones Theatre", coordinate: CLLocationCoordinate2D(latitude: 44.439642, longitude: -78.264975))
+        
+        ,
+        
+        Position(name: "Parking Lot 1", coordinate: CLLocationCoordinate2D(latitude: 44.438789, longitude: -78.264901))
+        
+        ,
+        
+        Position(name: "Parking Lot 2", coordinate: CLLocationCoordinate2D(latitude: 44.437691, longitude: -78.265790))
+        
+        ,
+        
+        Position(name: "Parking Lot 3", coordinate: CLLocationCoordinate2D(latitude: 44.440941, longitude: -78.264115))
+        
+        ,
+        
+        Position(name: "Parking Lot 4", coordinate: CLLocationCoordinate2D(latitude: 44.440165, longitude: -78.264203))
+    
+    ]
     
     @State var route: MKPolyline?
     
     var body: some View {
-        Map(coordinateRegion: $region, annotationItems: [bryanJonesTheatre]) { position in
-            MapMarker(coordinate: position.coordinate)
+        Map(coordinateRegion: $region, annotationItems: whereabout) { position in
+//                MapMarker(coordinate: position.coordinate)
+            MapAnnotation(coordinate: position.coordinate) {
+                HelperView(title: position.name)
+            }
         }
         .onAppear {
             MKMapView.appearance().mapType = .satelliteFlyover
         }
+        .ignoresSafeArea()
     }
 }
 
