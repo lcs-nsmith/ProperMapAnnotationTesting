138ef54195d03559c597d8168eec1beb4de891de
Nathan Smith <nsmith@lcs.on.ca>
2023-02-07 14:46:00 -0500

    Initial Commit

---
+++ ProperMapAnnotationTesting/ProperMapAnnotationTestingApp.swift
@@ -0,0 +1,17 @@
+//
+//  ProperMapAnnotationTestingApp.swift
+//  ProperMapAnnotationTesting
+//
+//  Created by Nathan on 2023-02-07.
+//
+
+import SwiftUI
+
+@main
+struct ProperMapAnnotationTestingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
