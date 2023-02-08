2c40b314eca01b2a51be7be82d298e1c99e9c83d
Russell Gordon <rgordon@lcs.on.ca>
2023-02-07 19:34:57 -0500

    Change to custom binding to highlight where issue lays with "undefined behaviour"

--- ProperMapAnnotationTesting/Views/CampusMap.swift
+++ ProperMapAnnotationTesting/Views/CampusMap.swift
@@ -9,12 +9,24 @@ import SwiftUI
 import MapKit
 
 struct CampusMap: View {
+    
     @State private var region = MKCoordinateRegion(center: CLLocationCoordinate2D(latitude: 44.439230, longitude: -78.265300), span: MKCoordinateSpan(latitudeDelta: 0.004, longitudeDelta: 0.004))
     
     @State var route: MKPolyline?
     
     var body: some View {
-        Map(coordinateRegion: $region, annotationItems: positions) { position in
+        
+        // Custom binding to highlight where issue lays with "undefined behaviour"
+        let region = Binding(
+            get: {
+                return self.region
+            },
+            set: { newValue in
+                self.region = newValue
+            }
+        )
+        
+        Map(coordinateRegion: region, annotationItems: positions) { position in
             MapAnnotation(coordinate: position.coordinate) {
                 Pin(title: position.name)
             }
